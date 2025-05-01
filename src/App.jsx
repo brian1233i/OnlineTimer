70897e854d4deda52921af7a83dffe4855df7906
brian1233i <brayanfamilia190@gmail.com>
2025-05-01 16:48:26 -0400

    upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="homepage">
+
+      <h1>Online timer</h1>
+      
+      <img src="https://media.istockphoto.com/id/1487963070/vector/futuristic-time-clock-hand-and-clock-face-digital-transformation-abstract-technology.jpg?s=612x612&w=0&k=20&c=vBMcZmwTPz_ofcnAAOQroZr4EIa5BfOIbnkqgIUoxxk="></img>
+      
+      <p>SEARCH BAR</p>
+      <input></input>
+      <button>chronometer</button>
+      <button> timer</button>
+      <div></div>
+      
+    </div>
+
+  );
+}
+
+export default App;
