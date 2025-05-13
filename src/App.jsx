c6155298820d3d5703cfdd1c2882b094b8d7d27f
brian1233i <brayanfamilia190@gmail.com>
2025-05-13 17:22:48 -0400

    change image and add new icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { AiOutlineBars } from "react-icons/ai";
+
 function App() {
   return (
     <div className="homepage">
 
-      <h1>Online timer</h1>
+      <h1>Online timer<AiOutlineBars /></h1>
       
-      <img src="https://media.istockphoto.com/id/1487963070/vector/futuristic-time-clock-hand-and-clock-face-digital-transformation-abstract-technology.jpg?s=612x612&w=0&k=20&c=vBMcZmwTPz_ofcnAAOQroZr4EIa5BfOIbnkqgIUoxxk="></img>
       
       <p>SEARCH BAR</p>
       <input></input>
@@ -14,7 +15,10 @@ function App() {
       
     </div>
 
+
+
   );
 }
 
 export default App;
+
