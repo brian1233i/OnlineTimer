772bdbbb196742393470d86e333a9a94e0b76f79
brian1233i <brayanfamilia190@gmail.com>
2025-05-14 17:31:55 -0400

    new icons images

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
 import { AiOutlineBars } from "react-icons/ai";
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+import { RxLapTimer } from "react-icons/rx";
+import { CountdownCircleTimer } from 'react-countdown-circle-timer'
+import { BiAlignLeft } from "react-icons/bi";
 
 function App() {
   return (
     <div className="homepage">
+
+ <CountdownCircleTimer
+    isPlaying
+    duration={7}
+    colors={['#004777', '#F7B801', '#A30000', '#A30000']}
+    colorsTime={[7, 5, 2, 0]}
+  >
+    {({ remainingTime }) => remainingTime}
+  </CountdownCircleTimer>
+  <BiAlignLeft />
 
       <h1>Online timer<AiOutlineBars /></h1>
       
